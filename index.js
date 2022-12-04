3ad92cc4bd77036e2bc4979f2b2259a068fa7e5c
Karan786-dev <techfire166@gmail.com>
2022-12-04 11:08:16 +0530

    nt

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express()
-const { lookup } = require('geoip-lite')
 const router = express.Router()
+let port = 8000
+
+app.use('/',router)
 
 router.get('/ip',(req,res) =>{
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(''+ip+'  ,,,, '+lookup(ip)+'')
 })
 
-app.use('/',router)
-app.listen(function(){
+app.listen(port,function(){
     console.log('Listening To The Server')
 })
